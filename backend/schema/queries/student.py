c59365da078e323e80bdcad4dd0214610e000558
Jessica Hernandez <126341255+jessherna@users.noreply.github.com>
2024-11-16 17:53:27 -0500

    Delete backend directory

--- backend/schema/queries/student.py
+++
@@ -1,8 +0,0 @@
-import graphene
-
-class Query(graphene.ObjectType):
-    get_student = graphene.String(id=graphene.Int())
-
-    def resolve_get_student(self, info, id):
-        # Mock implementation
-        return f"Student with ID: {id}"
--- backend/schema/schema.py
+++
@@ -1,5 +0,0 @@
-from graphene import Schema
-from schema.queries import Query
-from schema.mutations import Mutation
-
-schema = Schema(query=Query, mutation=Mutation)
